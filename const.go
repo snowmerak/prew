13ed7efed62c3455d37fc78cd7fcf5f03d2fae00
snowmerak <snowmerak@outlook.com>
2021-08-24 20:15:08 +0900

    Update const.go

--- const.go
+++ const.go
@@ -4,10 +4,17 @@ import "runtime"
 
 var python string
 
+var pip3 string
+var vpython string
+
 func init() {
 	if runtime.GOOS == "windows" {
 		python = "python"
+		pip3 = "bin/pip3.exe"
+		vpython = "bin/python.exe"
 	} else {
 		python = "python3"
+		pip3 = "bin/pip3"
+		vpython = "bin/python3"
 	}
 }
