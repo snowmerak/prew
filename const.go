6287915eb04f0076f364b26cc7a5224bbc699285
snowmerak <snowmerak@outlook.com>
2021-08-28 17:07:05 +0900

    Update const.go

--- const.go
+++ const.go
@@ -6,18 +6,15 @@ var python string
 
 var pip3 string
 var vpython string
-var activate string
 
 func init() {
 	if runtime.GOOS == "windows" {
 		python = "python"
 		pip3 = "Scripts\\pip3.exe"
 		vpython = "Scripts\\python.exe"
-		activate = "Scripts\\activate_this.py"
 	} else {
 		python = "python3"
 		pip3 = "bin/pip3"
 		vpython = "bin/python3"
-		activate = "bin/activate_this.py"
 	}
 }
